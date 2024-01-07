0765b78da02c52d2f4d059a76cc28819796642e5
Tami1118 <chenziyi93326@gmail.com>
2024-01-07 17:10:42 +0800

    environment construction

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -7,7 +7,7 @@
 
 <script>
 import { RouterView } from "vue-router";
-import AdminHeader from "../layouts/admin/AdminHeader"
+import AdminHeader from "@/layouts/admin/AdminHeader"
 
 export default {
   components: {
