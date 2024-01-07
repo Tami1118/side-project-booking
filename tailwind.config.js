1a030c1f2f68caa0eb45739c815164033c057da9
Tami1118 <chenziyi93326@gmail.com>
2024-01-07 17:16:51 +0800

    environment construction

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    // "./src/**/*.{vue,js,ts,jsx,tsx, scss}",
+    "./src/**/*.{vue,js,ts,jsx,tsx, scss}",
     "./src/components/**/*.{vue,js,ts,jsx,tsx, scss}",
     "./src/views/**/*.{vue,js,ts,jsx,tsx, scss}",
   ],
