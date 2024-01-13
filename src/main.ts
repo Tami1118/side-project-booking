0d3459dc28e3d6c6222a8d90b100bd0cb694b86a
kelvin <kelvin80121@hotmail.com>
2024-01-14 00:01:39 +0800

    update login

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import './assets/scss/all.scss'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import { registerPlugins } from "@/plugins"
 import App from './App.vue'
-import router from './router'
-
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 
 const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.use(VueAxios, axios)
-
+registerPlugins(app)
 app.mount('#app')
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -2,7 +2,7 @@
   <div class="grid grid-cols-1 md:grid-cols-2 bg-neutral-100 h-screen">
     <div class="col hidden md:block">
       <img class="h-screen w-full object-cover object-center overflow-hidden" src="../assets/images/desktop/register.png" />
-    </div>
+    </div>   
     <div class="col relative overflow-hidden">
       <div class="absolute top-0 left-0 pt-8 md:pt-18">
         <svg class="max-[374px]:hidden" width="960" height="187" viewBox="0 0 960 187" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -56,47 +56,105 @@
           </defs>
         </svg>
       </div>
-
       <div class="h-full flex justify-center items-center px-5 lg:px-0">
         <div class="basis-full lg:basis-1/2 flex flex-col gap-10 duration-300">
           <div class="text-bold z-10">
             <div class="text-primary-100 font-bold text-3h md:text-4 mb-2">享樂酒店，誠摯歡迎</div>
-            <h1 class="text-white font-bold text-8 xl:text-12 duration-300">立即開始旅程</h1>
+            <h1 class="text-white font-bold text-8 xl:text-12 duration-300">立即註冊</h1>
           </div>
-          <div class="flex flex-col gap-4">
-            <div class="flex flex-col gap-2">
-              <label for="user-email" class="font-bold text-white">電子信箱</label>
-              <input 
-                type="email" 
-                id="user-email" 
-                class="p-4 rounded-2" 
-                placeholder="請輸入電子信箱"
-                v-model="loginData.email"
-              >
-            </div>
-            <div class="flex flex-col gap-2">
-              <label for="user-password" class="font-bold text-white">密碼</label>
-              <input 
-                type="email" 
-                id="user-password" 
-                class="p-4 rounded-2" 
-                placeholder="請輸入密碼"
-                v-model="loginData.password"
-              >
-            </div>
-            <div class="flex justify-between">
-              <div>
-                <input type="checkbox" id="remember" class="p-4 rounded-2 me-2">
-                <label for="remember" class="font-bold text-3h md:text-4 text-white">記住帳號</label>
+
+          <div class="flex justify-around z-10">
+            <div class="text-white" @click="signupStep = 1">1</div>
+            <div class="text-white" @click="signupStep = 2">2</div>
+          </div>
+          
+          <!-- step1 -->
+          <div v-if="signupStep === 1">
+            <div class="flex flex-col gap-4">
+              <div class="flex flex-col gap-2">
+                <label for="user-email-signup" class="font-bold text-white">電子信箱</label>
+                <input 
+                  type="email" 
+                  id="user-email-signup" 
+                  class="p-4 rounded-2" 
+                  placeholder="請輸入電子信箱"
+                  v-model="signupData.email"
+                >
               </div>
-              <a href="#" class="block btn-text text-3h md:text-4 underline decoration-1">忘記密碼</a>
+              <div class="flex flex-col gap-2">
+                <label for="user-password" class="font-bold text-white">密碼</label>
+                <input 
+                  type="email" 
+                  id="user-password" 
+                  class="p-4 rounded-2" 
+                  placeholder="請輸入密碼"
+                  v-model="signupData.password"
+                >
+              </div>
+              <div class="flex flex-col gap-2">
+                <label for="user-password" class="font-bold text-white">確認密碼</label>
+                <input 
+                  type="email" 
+                  id="user-password" 
+                  class="p-4 rounded-2" 
+                  placeholder="請輸入密碼"
+                  v-model="signupData.password"
+                >
+              </div>
+  
+  
+  
+            </div>
+            <div class="btn-primary font-bold btn text-center mt-10" @click="signupStep = 2">
+              下一步
             </div>
           </div>
-          <div class="btn-primary font-bold btn text-center" @click="login()">會員登入</div>
+          <!-- step2 -->
+          <div v-if="signupStep === 2">
+            <div class="flex flex-col gap-4">
+              <div class="flex flex-col gap-2">
+                <label for="user-name" class="font-bold text-white">姓名</label>
+                <input 
+                  type="text"  
+                  id="user-name" 
+                  class="p-4 rounded-2" 
+                  placeholder="請輸入姓名"
+                  v-model="signupData.name"
+                >
+              </div>
+              <div class="flex flex-col gap-2">
+                <label for="user-phone" class="font-bold text-white">手機號碼</label>
+                <input 
+                  type="phone"  
+                  id="user-phone" 
+                  class="p-4 rounded-2" 
+                  placeholder="請輸入手機號碼"
+                  v-model="signupData.phone"
+                >
+              </div>
+              <!-- <div class="flex flex-col gap-2">
+                <label for="user-password" class="font-bold text-white">確認密碼</label>
+                <input 
+                  type="email" 
+                  id="user-password" 
+                  class="p-4 rounded-2" 
+                  placeholder="請輸入密碼"
+                  v-model="passwordConfirm"
+                >
+              </div> -->
+  
+  
+            </div>
+            <div class="btn-primary font-bold btn text-center mt-10" @click="signup()">
+              完成註冊
+            </div>
+          </div>
+
+
           <div class="text-white flex">
-            還沒有會員嗎？
-            <span class="btn-text underline decoration-1 ms-2" @click="$router.push('/signup')">
-              前往註冊
+            已經有會員了嗎？
+            <span class="btn-text underline decoration-1 ms-2" @click="$router.push('/login')">
+              立即登入
             </span>
           </div>
         </div>
@@ -110,6 +168,6 @@ import { storeToRefs } from'pinia'
 import { useUserStore } from '@/stores/userStore'
 
 const userStore = useUserStore()
-const { loginData } = storeToRefs(userStore)
-const login = userStore.login
+const { signupStep, signupData, passwordConfirm } = storeToRefs(userStore)
+const signup = userStore.signup
 </script>
