8e563a2605918b71ffe544f1073dbb459210d594
Tami1118 <chenziyi93326@gmail.com>
2024-02-04 20:26:49 +0800

    deploy

---
+++ assets/CulinaryAdmin-J3KCN4aG.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-x3n3nnut.js";import{c as r,b as c}from"./index-oxIgGxWH.js";const n={};function o(t,a){return c(),r("div",null," 我是美味佳餚管理頁 ")}const f=e(n,[["render",o]]);export{f as default};
