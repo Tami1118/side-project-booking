d794eeb57923842ff8c9c576dd5bae7ef8b5e514
Tami1118 <chenziyi93326@gmail.com>
2024-02-04 16:21:14 +0800

    :triangular_flag_on_post: Update

--- src/stores/cityStore.ts
+++ src/stores/cityStore.ts
@@ -5,12 +5,12 @@
 // const { VITE_URL } = import.meta.env
 // import { useRouter } from 'vue-router'
 
-export const useTestStore = defineStore('testStore', () => {
-  const router = useRouter()
+// export const useTestStore = defineStore('testStore', () => {
+//   const router = useRouter()
 
   
 
-  return {
+//   return {
 
-  }
-})
+//   }
+// })
