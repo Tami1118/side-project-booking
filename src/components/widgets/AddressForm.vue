a1626f12bd893ada0d2ae81d2db4666893db7651
Tami1118 <chenziyi93326@gmail.com>
2024-02-13 11:58:35 +0800

    :triangular_flag_on_post: Fix: address bug fix

--- src/components/widgets/AddressForm.vue
+++ src/components/widgets/AddressForm.vue
@@ -1,33 +1,20 @@
 <script setup lang="ts">
 // Basic
-import { ref, watch, computed, onMounted } from "vue";
+import { ref, watch, computed } from "vue";
 import { storeToRefs } from "pinia";
-import axios from "axios";
 
-import type { City, District } from "@/interfaces/country";
+import type { District } from "@/interfaces/country";
 
 // Order-地址
 import { useOrderStore } from "@/stores/orderStore"
 const orderStore = useOrderStore()
 const { selectDistrict } = storeToRefs(orderStore)
-
-// 台灣縣市API
-const taiwanCities = ref<null|City[]>(null);
-const getTaiwanCity = async () => {
-  try {
-    const url = "/api/abc873693/2804e64324eaaf26515281710e1792df/raw/a1e1fc17d04b47c564bbd9dba0d59a6a325ec7c1/taiwan_districts.json";
-    const res = await axios.get(url)
-    taiwanCities.value = res.data
-    // console.log("取得縣市列表", taiwanCities.value)
-  } catch (err) {
-    console.log(err)
-  }
-}
+import taiwanCities from '@/data/taiwanCities'
 
 // 縣市列表
 const cities = computed(() => {
-  if (taiwanCities.value) {
-    return taiwanCities.value.map((item) => item.name);
+  if (taiwanCities) {
+    return taiwanCities.map((item) => item.name);
   } else {
     return []
   }
@@ -35,8 +22,8 @@ const cities = computed(() => {
 
 // 行政區列表
 const districts = computed<District[]>(() => {
-  if (taiwanCities.value) {
-    const city = taiwanCities.value.find((item) => item.name === selectCity.value);
+  if (taiwanCities) {
+    const city = taiwanCities.find((item) => item.name === selectCity.value);
     return city ? city.districts : [];
   } else {
     return []
@@ -46,10 +33,6 @@ const districts = computed<District[]>(() => {
 // 縣市變動時，行政區隨縣市列表變動，預設為 ""
 const selectCity = ref<string>("")
 watch(selectCity, (n) => { if(n) selectDistrict.value = "" })
-
-onMounted(() => {
-  getTaiwanCity();
-});
 </script>
 
 <template>
