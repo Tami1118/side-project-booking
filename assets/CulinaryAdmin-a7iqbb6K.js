7bc077a8480936c455dbe31f11436e83a1d052ef
Tami1118 <chenziyi93326@gmail.com>
2024-02-19 19:58:28 +0800

    deploy

--- assets/CulinaryAdmin-a7iqbb6K.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-x3n3nnut.js";import{e as r,c}from"./index-Bx7n4dr3.js";const n={};function o(t,a){return c(),r("div",null," 我是美味佳餚管理頁 ")}const f=e(n,[["render",o]]);export{f as default};
---
+++ assets/bgwave-zj1l6dP5.js
@@ -0,0 +1,1 @@
+const s="/side-project-booking/assets/register-yJzPYipC.png",o="/side-project-booking/svg/bgwave.svg";export{s as _,o as a};
