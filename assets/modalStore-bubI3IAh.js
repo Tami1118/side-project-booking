60b36ce502c66bc56607e059b65a5dfb36270fa9
Tami1118 <chenziyi93326@gmail.com>
2024-02-20 20:15:46 +0800

    deploy

---
+++ assets/modalStore-bubI3IAh.js
@@ -0,0 +1,1 @@
+import{q as e,r as a}from"./index-_3h-oLcu.js";const t=e("modalStore",()=>{const o=a(!1);return{isModalOpen:o,openModal:()=>{o.value=!0},closeModal:()=>{o.value=!1}}});export{t as u};
